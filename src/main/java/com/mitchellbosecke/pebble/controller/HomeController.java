79e4367bff071473fce92b6d9e3b0f7deab2c3fa
Mitchell Bosecke <mitchellbosecke@gmail.com>
2013-09-30 22:28:36 -0600

    Added basic structure to website

--- src/main/java/com/mitchellbosecke/pebble/controller/HomeController.java
+++ src/main/java/com/mitchellbosecke/pebble/controller/HomeController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value="/pebble")
+@RequestMapping(value="/")
 public class HomeController {
 	
 	@RequestMapping(method=RequestMethod.GET)
 	public String getView() {
-		return "pebble";
+		return "home";
 	}
 }
